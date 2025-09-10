1b0fde78f856349604b71ea1a7d8756c89dfc06f
hlpdev <hunterpollock2006@gmail.com>
2025-09-10 15:24:50 -0400

    changed the entry_path from const uint8_t* to const char*
    added parameters for capturing argc and argv for later implementation in the lua state

--- engine/include/Umbra/umbra.hpp
+++ engine/include/Umbra/umbra.hpp
@@ -7,7 +7,7 @@
 
 namespace umbra {
 
-    UMBRA_API int umbra_run(const int8_t* entry_path, const uint8_t* secret, size_t secret_size);
+    UMBRA_API int umbra_run(const char* entry_path, const uint8_t* secret, size_t secret_size, int argc, char** argv);
 
     [[noreturn]] UMBRA_API void umbra_fail(const std::string& message);
     [[noreturn]] UMBRA_API void umbra_fail(const char* message);
--- engine/src/umbra.cpp
+++ engine/src/umbra.cpp
@@ -1,9 +1,10 @@
 #include "Umbra/umbra.hpp"
 
+#include <iostream>
 #include <stdexcept>
 
-int umbra::umbra_run(const int8_t* entry_path, const uint8_t* secret, size_t secret_size) {
-
+int umbra::umbra_run(const char* entry_path, const uint8_t* secret, size_t secret_size, int argc, char** argv) {
+    std::cout << entry_path << std::endl;
     return 0;
 }
 
