e6758c80aa10826e5b355452a2f3dded0024a894
hlpdev <hunterpollock2006@gmail.com>
2025-09-12 18:46:59 -0400

    include cstring in boot.cpp

--- engine/src/boot/boot.cpp
+++ engine/src/boot/boot.cpp
@@ -1,5 +1,7 @@
 #include "Umbra/boot.hpp"
 
+#include <cstring>
+
 umbra::EmbeddedFooter umbra::make_footer(const uint32_t key_length) {
   EmbeddedFooter footer{};
   std::memcpy(footer.magic, SECRET_KEY_MAGIC, SECRET_KEY_MAGIC_LEN);
