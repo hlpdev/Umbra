d7707a67f78e21b8c7a07fc18507569319ad408d
hlpdev <hunterpollock2006@gmail.com>
2025-09-13 00:06:52 -0400

    reorganized the service registry

--- engine/include/Umbra/services.hpp
+++ engine/include/Umbra/services.hpp
@@ -11,14 +11,24 @@ using namespace std::string_literals;
 namespace umbra {
 
   struct IService {
+
     virtual ~IService() = default;
+
   };
 
   class UMBRA_API ServiceRegistry final {
   public:
 
+    explicit ServiceRegistry(const std::shared_ptr<sol::state>& lua_state) : lua_state_(lua_state) {}
+    ~ServiceRegistry() = default;
+
+    ServiceRegistry(const ServiceRegistry&) = delete;
+    ServiceRegistry& operator=(const ServiceRegistry&) = delete;
+    ServiceRegistry(ServiceRegistry&&) = delete;
+    ServiceRegistry& operator=(ServiceRegistry&&) = delete;
+
     template<class T, class... Args>
-    std::shared_ptr<T> emplace(const std::string& name, Args&&... args) {
+    std::shared_ptr<T> register_service(const std::string& name, Args&&... args) {
       static_assert(std::is_base_of_v<IService, T>, "Registered services must inherit from IService");
 
       auto service = std::make_shared<T>(std::forward<Args>(args)...);
@@ -27,7 +37,7 @@ namespace umbra {
     }
 
     template<class T>
-    std::shared_ptr<T> get(const std::string& name) const {
+    std::shared_ptr<T> fetch_service(const std::string& name) const {
       static_assert(std::is_base_of_v<IService, T>, "Registered services must inherit from IService");
 
       const auto it = services_.find(name);
@@ -36,6 +46,8 @@ namespace umbra {
 
   private:
     std::unordered_map<std::string, std::shared_ptr<IService>> services_;
+
+    std::shared_ptr<sol::state> lua_state_;
   };
 
 }
