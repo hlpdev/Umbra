b42731b5727a6e0bea136bf2843ac880e2d9458f
hlpdev <hunterpollock2006@gmail.com>
2025-09-17 11:45:43 -0400

    luadefs for vector2 and 3

---
+++ luadefs/vector2.d.lua
@@ -0,0 +1,81 @@
+---@meta
+---@diagnostic disable: missing-return
+
+---@class Vector2 : userdata
+---@field x number
+---@field y number
+local Vector2 = {}
+
+---Creates a Vector2 with the specified X and Y values.
+---@overload fun(): Vector2
+---@param x number
+---@param y number
+---@return Vector2
+function Vector2.new(x, y) end
+
+---Absolute value per component.
+---@return Vector2
+function Vector2:abs() end
+
+---Ceil per component.
+---@return Vector2
+function Vector2:ceil() end
+
+---Floor per component.
+---@return Vector2
+function Vector2:floor() end
+
+---Component-wise max.
+---@param other Vector2
+---@return Vector2
+function Vector2:max(other) end
+
+---Component-wise min.
+---@param other Vector2
+---@return Vector2
+function Vector2:min(other) end
+
+---Length (magnitude)
+---@return number
+function Vector2:length() end
+
+---2D cross product (scalar z component)
+---@param other Vector2
+---@return number
+function Vector2:cross(other) end
+
+---Component-wise sign (-1, 0, +1)
+---@return Vector2
+function Vector2:sign() end
+
+---Dot product.
+---@param other Vector2
+---@return number
+function Vector2:dot(other) end
+
+---Angle to other vector.
+---@param other Vector2
+---@param is_signed boolean # if true, signed angle via atan2
+---@return number
+function Vector2:angle(other, is_signed) end
+
+---Linear interpolation.
+---@param other Vector2
+---@param alpha number
+---@return Vector2
+function Vector2:lerp(other, alpha) end
+
+---Fuzzy equality by epsilon.
+---@param other Vector2
+---@param epsilon number|nil
+---@return boolean
+function Vector2:fuzzy_eq(other, epsilon) end
+
+---@operator add(Vector2): Vector2
+---@operator sub(Vector2): Vector2
+---@operator mul(number): Vector2
+---@operator mul(Vector2): Vector2
+---@operator div(number): Vector2
+---@operator div(Vector2): Vector2
+---@operator unm(): Vector2
+---@operator len(): number
---
+++ luadefs/vector3.d.lua
@@ -0,0 +1,80 @@
+---@meta
+---@diagnostic disable: missing-return
+
+---@class Vector3 : userdata
+---@field x number
+---@field y number
+---@field z number
+local Vector3 = {}
+
+---Creates a Vector3 with the specified X, Y, and Z values.
+---@overload fun(): Vector3
+---@param x number
+---@param y number
+---@param z number
+---@return Vector3
+function Vector3.new(x, y, z) end
+
+---Absolute value per component.
+---@return Vector3
+function Vector3:abs() end
+
+---Ceil per component.
+---@return Vector3
+function Vector3:ceil() end
+
+---Floor per component.
+---@return Vector3
+function Vector3:floor() end
+
+---Component-wise max.
+---@param other Vector3
+---@return Vector3
+function Vector3:max(other) end
+
+---Compoent-wise min.
+---@param other Vector3
+---@return Vector3
+function Vector3:min(other) end
+
+---Length (magnitude)
+---@return number
+function Vector3:length() end
+
+---Dot product.
+---@param other Vector3
+---@return number
+function Vector3:dot(other) end
+
+---Cross product.
+---@param other Vector3
+---@return Vector3
+function Vector3:cross(other) end
+
+---Unsigned or signed angle (with axis).
+---@overload fun(other: Vector3): number
+---@param other Vector3
+---@param axis Vector3
+---@return number
+function Vector3:angle(other, axis) end
+
+---Linear interpolation.
+---@param other Vector3
+---@param alpha number
+---@return Vector3
+function Vector3:lerp(other, alpha) end
+
+---Fuzzy equality by epsilon.
+---@param other Vector3
+---@param epsilon number|nil
+---@return boolean
+function Vector3:fuzzy_eq(other, epsilon) end
+
+---@operator add(Vector3): Vector3
+---@operator sub(Vector3): Vector3
+---@operator mul(number): Vector3
+---@operator mul(Vector3): Vector3
+---@operator div(number): Vector3
+---@operator div(Vector3): Vector3
+---@operator unm(): Vector3
+---@operator len(): number
