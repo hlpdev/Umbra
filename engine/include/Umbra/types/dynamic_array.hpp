e5b2a44349ebf1855a8de32b805383fcd95c2a71
hlpdev <hunterpollock2006@gmail.com>
2025-09-17 11:46:40 -0400

    quick refactor

--- engine/include/Umbra/types/dynamic_array.hpp
+++ engine/include/Umbra/types/dynamic_array.hpp
@@ -11,26 +11,28 @@
 namespace umbra {
 
   struct UMBRA_API DynamicArray final : IType {
-    std::vector<sol::object> data;
+  private:
+    std::vector<sol::object> data_;
 
+  public:
     const char* name() override { return "DynamicArray"; }
 
     DynamicArray() noexcept = default;
 
-    int size() const noexcept { return static_cast<int>(data.size()); }
-    int capacity() const noexcept { return static_cast<int>(data.capacity()); }
-    bool empty() const noexcept { return data.empty(); }
+    int size() const noexcept { return static_cast<int>(data_.size()); }
+    int capacity() const noexcept { return static_cast<int>(data_.capacity()); }
+    bool empty() const noexcept { return data_.empty(); }
 
-    void clear() noexcept { data.clear(); }
-    void reserve(const int size) { data.reserve(std::abs(size)); }
-    void shrink_to_fit() { data.shrink_to_fit(); }
+    void clear() noexcept { data_.clear(); }
+    void reserve(const int size) { data_.reserve(std::abs(size)); }
+    void shrink_to_fit() { data_.shrink_to_fit(); }
 
-    sol::object get(const int index, sol::this_state this_state) const noexcept {
+    sol::object get(const int index, const sol::this_state this_state) const noexcept {
       if (index < 1 || index > size()) {
         return make_object(this_state, sol::lua_nil);
       }
 
-      return data[static_cast<size_t>(index - 1)];
+      return data_[static_cast<size_t>(index - 1)];
     }
 
     void set(const int index, sol::object value) noexcept {
@@ -38,29 +40,29 @@ namespace umbra {
         return;
       }
 
-      data[static_cast<size_t>(index - 1)] = std::move(value);
+      data_[static_cast<size_t>(index - 1)] = std::move(value);
     }
 
-    void push_back(sol::object value) { data.emplace_back(std::move(value)); }
-    void push_front(sol::object value) { data.emplace(data.begin(), std::move(value)); }
+    void push_back(sol::object value) { data_.emplace_back(std::move(value)); }
+    void push_front(sol::object value) { data_.emplace(data_.begin(), std::move(value)); }
 
     sol::object pop_back(const sol::this_state this_state) noexcept {
-      if (data.empty()) {
+      if (data_.empty()) {
         return make_object(this_state, sol::lua_nil);
       }
 
-      sol::object value = std::move(data.back());
-      data.pop_back();
+      sol::object value = std::move(data_.back());
+      data_.pop_back();
       return value;
     }
 
     sol::object pop_front(const sol::this_state this_state) noexcept {
-      if (data.empty()) {
+      if (data_.empty()) {
         return make_object(this_state, sol::lua_nil);
       }
 
-      sol::object value = std::move(data.front());
-      data.erase(data.begin());
+      sol::object value = std::move(data_.front());
+      data_.erase(data_.begin());
       return value;
     }
 
@@ -68,9 +70,9 @@ namespace umbra {
       const size_t data_size = size();
 
       if (index >= 1 && index <= static_cast<int>(data_size + 1)) {
-        data.insert(data.begin() + (index - 1), std::move(value));
+        data_.insert(data_.begin() + (index - 1), std::move(value));
       } else {
-        data.emplace_back(std::move(value));
+        data_.emplace_back(std::move(value));
       }
     }
 
@@ -80,14 +82,14 @@ namespace umbra {
         return;
       }
 
-      data.erase(data.begin() + (index - 1));
+      data_.erase(data_.begin() + (index - 1));
     }
 
     DynamicArray concat(const DynamicArray& other) const noexcept {
       DynamicArray out;
-      out.data.reserve(data.size() + other.data.size());
-      out.data.insert(out.data.end(), data.begin(), data.end());
-      out.data.insert(out.data.end(), other.data.begin(), other.data.end());
+      out.data_.reserve(data_.size() + other.data_.size());
+      out.data_.insert(out.data_.end(), data_.begin(), data_.end());
+      out.data_.insert(out.data_.end(), other.data_.begin(), other.data_.end());
       return out;
     }
 
@@ -95,33 +97,33 @@ namespace umbra {
       DynamicArray out;
       const size_t size = table.size();
 
-      out.data.reserve(size);
+      out.data_.reserve(size);
 
       for (size_t i = 1; i <= size; ++i) {
-        out.data.emplace_back(table.get<sol::object>(i));
+        out.data_.emplace_back(table.get<sol::object>(i));
       }
 
       return out;
     }
 
     sol::as_table_t<std::vector<sol::object>> to_table() const {
-      return as_table(data);
+      return as_table(data_);
     }
 
     bool equals(const DynamicArray& other, const sol::this_state this_state) const {
-      if (data.size() != other.data.size()) {
+      if (data_.size() != other.data_.size()) {
         return false;
       }
 
       sol::state_view state_view(this_state);
-      sol::function raw_equal = state_view["rawequal"];
-      for (size_t i = 0; i < data.size(); ++i) {
+      const sol::function raw_equal = state_view["rawequal"];
+      for (size_t i = 0; i < data_.size(); ++i) {
         if (raw_equal.valid()) {
-          if (!raw_equal(data[i], other.data[i])) {
+          if (!raw_equal(data_[i], other.data_[i])) {
             return false;
           }
         } else {
-          if (data[i] != other.data[i]) {
+          if (data_[i] != other.data_[i]) {
             return false;
           }
         }
@@ -136,13 +138,18 @@ namespace umbra {
       std::ostringstream string_stream;
 
       string_stream << "DynamicArray(";
-      for (size_t i = 0; i < data.size(); ++i) {
+      for (size_t i = 0; i < data_.size(); ++i) {
         if (i) {
           string_stream << ", ";
         }
 
+        if (data_[i].is<std::string>()) {
+          string_stream << '\'' << data_[i].as<std::string>() << '\'';
+          continue;
+        }
+
         if (to_string.valid()) {
-          sol::object value = to_string(data[i]);
+          sol::object value = to_string(data_[i]);
           if (value.is<std::string>()) {
             string_stream << value.as<std::string>();
           } else {
@@ -206,6 +213,40 @@ namespace umbra {
         }
 
         return make_object(this_state, sol::lua_nil);
+      };
+
+      user_type[sol::meta_function::ipairs] = [](const DynamicArray& dynamic_array, const sol::this_state this_state) {
+        auto iter = [this_state](const DynamicArray& dynamic_array_to_iter, const int i) -> std::tuple<sol::object, sol::object> {
+          const int next = i + 1;
+          if (next > dynamic_array_to_iter.size()) {
+            sol::object nil = make_object(this_state, sol::lua_nil);
+            return { nil, nil };
+          }
+
+          sol::object value = dynamic_array_to_iter.get(next, this_state);
+          if (value.is<sol::lua_nil_t>()) {
+            sol::object nil = make_object(this_state, sol::lua_nil);
+            return { value, nil };
+          }
+
+          return { make_object(this_state, next), value };
+        };
+
+        return std::make_tuple(iter, std::ref(dynamic_array), 0);
+      };
+
+      user_type[sol::meta_function::pairs] = [](const DynamicArray& dynamic_array, const sol::this_state this_state) {
+        auto iter = [this_state](const DynamicArray& dynamic_array_to_iter, const int i) -> std::tuple<sol::object, sol::object> {
+          const int next = i + 1;
+          if (next > dynamic_array_to_iter.size()) {
+            sol::object nil = make_object(this_state, sol::lua_nil);
+            return { nil, nil };
+          }
+
+          return { make_object(this_state, next), dynamic_array_to_iter.get(next, this_state) };
+        };
+
+        return std::make_tuple(iter, std::ref(dynamic_array), 0);
       };
     }
   };
